9cd3c8236c5597d7569f1c29af76040edbb17edf
Sasha <sasha@min.studio>
2022-07-07 19:23:15 +0300

    feat: add services

---
+++ src/components/Cards/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'gatsby'
+import * as s from './Cards.module.scss'
+
+const Cards = ({ data }) => {
+  return (
+    <ul className={s.cards}>
+      {data.map(({ node: { slug, name, duration, preview } }) => (
+        <li className={s.cards_item}>
+          <div className={s.cards_heading}>
+            <span className={s.cards_name}>{name}</span>
+            <span className={s.cards_label}>{duration}</span>
+          </div>
+          <div className={s.cards_pic}>
+            <img src={preview.publicURL} alt={name} />
+          </div>
+          <div className={s.cards_btns}>
+            <Button as={Link} to={`/services/${slug}`}>
+              Записатись
+            </Button>
+            <Button as={Link} variant="secondary" to={`/services/${slug}`}>
+              Детальніше
+            </Button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default Cards
---
+++ src/containers/Home/components/Services/constants.js
@@ -0,0 +1,61 @@
+const DATA = {
+  title: 'Послуги',
+  descr:
+    'Наші косметологи з великим досвідом роботи здатні створювати класні результати, виключаючи негативні властивості та небажані наслідки. Довірте себе в руки професіоналів краси.',
+  list: [
+    {
+      name: 'Ботулінотерапія',
+      duration: 'до 30 хв',
+      pic: 'botox',
+      link: '#product',
+    },
+    {
+      name: 'Чистка обличчя ',
+      duration: '1-1:15 год',
+      pic: 'facial-cleansing',
+      link: '#product',
+    },
+    {
+      name: 'Контурна пластика губ',
+      duration: 'до 30 хв',
+      pic: 'lips',
+      link: '#product',
+    },
+    {
+      name: 'Карбоксітерапія',
+      duration: '45-60 хв',
+      pic: 'carboxytherapy',
+      link: '#product',
+    },
+    {
+      name: 'Мезотерапія волосяної частини голови',
+      duration: '60 хв',
+      pic: 'mesotherapy-scalp',
+      link: '#product',
+    },
+    {
+      name: 'Пілінг',
+      duration: '20-30 хв',
+      pic: 'peeling',
+      link: '#product',
+    },
+    {
+      name: 'Мезотерапія',
+      duration: '30 хв',
+      pic: 'mesotherapy',
+      link: '#product',
+    },
+    {
+      name: 'Процедура «Ice&Fire»',
+      duration: 'до 60 хв',
+      pic: 'ice-fire',
+      link: '#product',
+    },
+  ],
+  btn: {
+    text: 'Всі послуги',
+    link: '/services',
+  },
+}
+
+export default DATA
