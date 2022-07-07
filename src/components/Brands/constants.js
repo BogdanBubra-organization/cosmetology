4644539208c48a1fd7f1bba7ddc292c97a618d1c
Sasha <sasha@min.studio>
2022-07-07 12:20:54 +0300

    feat: add hero

---
+++ src/components/Brands/constants.js
@@ -0,0 +1,12 @@
+const DATA = {
+  title: 'Бренди з якими ми працюємо',
+  list: [
+    { key: 'evanthia', alt: 'Evanthia' },
+    { key: 'juvederm', alt: 'Juvederm' },
+    { key: 'filorga', alt: 'Filorga' },
+    { key: 'zoskinhealth', alt: 'Zoskinhealth' },
+    { key: 'radiesse', alt: 'Radiesse' },
+  ],
+}
+
+export default DATA
--- src/containers/Home/components/Hero/index.js
+++ src/containers/Home/components/Hero/index.js
@@ -1,23 +1,41 @@
 import React from 'react'
 import { Button, Container } from 'react-bootstrap'
+import { StaticImage } from 'gatsby-plugin-image'
 import Icon from '~components/Icon'
+import Brands from '~components/Brands'
 import DATA from './constants'
 import * as s from './Hero.module.scss'
 
 const Hero = () => {
   const { title, descr, btnOrder, btnServices } = DATA
+
   return (
     <section className={s.hero}>
-      <Container className={s.hero_inner}>
-        <h1 className={s.hero_title}>{title}</h1>
-        <p className={s.hero_descr}>{descr}</p>
-        <div className={s.hero_btns}>
-          <Button className="btn-icon">
-            <Icon name="tel" size={20} />
-            <span className="btn-icon-text">{btnOrder}</span>
-          </Button>
-          <Button variant="secondary">{btnServices}</Button>
+      <Container className={s.hero_container}>
+        <div className={s.hero_inner}>
+          <div className={s.hero_content}>
+            <h1 className={s.hero_title}>{title}</h1>
+            <p className={s.hero_descr}>{descr}</p>
+            <div className={s.hero_btns}>
+              <Button className="btn-icon">
+                <Icon name="tel" size={20} />
+                <span className="btn-icon-text">{btnOrder}</span>
+              </Button>
+              <Button variant="secondary">{btnServices}</Button>
+            </div>
+          </div>
+          <div className={s.hero_ill}>
+            <StaticImage
+              src="./img/hero.jpg"
+              alt="Надання послуги"
+              quality={100}
+              placeholder="none"
+              loading="eager"
+            />
+            <span className={s.hero_ill_line} />
+          </div>
         </div>
+        <Brands />
       </Container>
     </section>
   )
