d20fe20795ee4b48dc156dc0003a1a2b328af5c5
Sasha <sasha@min.studio>
2022-07-13 13:51:19 +0300

    feat: modals, dropdown, adaptive header & footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,69 @@
-import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Container, Dropdown } from 'react-bootstrap'
 import { useLocation } from '@gatsbyjs/reach-router'
 import { withPrefix, Link } from 'gatsby'
+import useMatchMedia from '~hooks/useMatchMedia'
 import PropTypes from 'prop-types'
-import cn from 'classnames'
+import ModalOrder from '~components/ModalOrder'
 import logo from '~img/logo.svg'
 import Menu from '~components/Menu'
 import * as s from './style.module.scss'
 
 const Logo = ({ alt }) => <img width="244" height="52" src={logo} alt={alt} />
 
+const ButtonAsk = ({ handleShowOrder }) => (
+  <Button onClick={handleShowOrder} variant="secondary">
+    Маєте питання?
+  </Button>
+)
+
 const Header = ({ siteTitle }) => {
   const location = useLocation()
   const isHomepage = location.pathname === withPrefix('/')
 
+  const [showOrder, setShowOrder] = useState(false)
+
+  const handleShowOrder = () => setShowOrder(true)
+
+  const isMdDown = useMatchMedia('(max-width: 767px)')
+
   return (
-    <Container as="header" className={cn(s.header, 'animate')}>
-      {isHomepage ? (
-        <Logo alt={siteTitle} />
-      ) : (
-        <Link to="/">
+    <Container as="header" className="animate">
+      <div className={s.header}>
+        {isHomepage ? (
           <Logo alt={siteTitle} />
-        </Link>
-      )}
-      <Menu variant="header" />
-      <div className={s.header_btn}>
-        <Button variant="secondary">Маєте питання?</Button>
+        ) : (
+          <Link to="/">
+            <Logo alt={siteTitle} />
+          </Link>
+        )}
+
+        <Menu variant="header" />
+
+        {!isMdDown && (
+          <div className={s.header_btn}>
+            <ButtonAsk handleShowOrder={handleShowOrder} />
+          </div>
+        )}
+
+        <Dropdown>
+          <Dropdown.Toggle className="dropdown-toggle" as="button" />
+          <Dropdown.Menu
+            popperConfig={{
+              modifiers: [{ name: 'offset', options: { offset: [0, 16] } }],
+            }}
+          >
+            <Menu variant="dropdown" />
+            {isMdDown && <ButtonAsk handleShowOrder={handleShowOrder} />}
+          </Dropdown.Menu>
+        </Dropdown>
+        <div className="dropdown-overlay" />
       </div>
+      <ModalOrder
+        variant="ask"
+        show={showOrder}
+        onHide={() => setShowOrder(false)}
+      />
     </Container>
   )
 }
--- src/containers/Home/components/Hero/index.js
+++ src/containers/Home/components/Hero/index.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 import Icon from '~components/Icon'
 import Brands from '~components/Brands'
+import ModalOrder from '~components/ModalOrder'
 import DATA from './constants'
 import * as s from './Hero.module.scss'
 
 const Hero = () => {
   const { title, descr, btnOrder, btnServices } = DATA
+
+  const [showOrder, setShowOrder] = useState(false)
 
   return (
     <section className={s.hero}>
@@ -17,11 +21,13 @@ const Hero = () => {
             <h1 className={s.hero_title}>{title}</h1>
             <p className={s.hero_descr}>{descr}</p>
             <div className={s.hero_btns}>
-              <Button className="btn-icon">
+              <Button className="btn-icon" onClick={() => setShowOrder(true)}>
                 <Icon name="tel" size={20} />
                 <span className="btn-icon-text">{btnOrder}</span>
               </Button>
-              <Button variant="secondary">{btnServices}</Button>
+              <Button as={Link} to="/services" variant="secondary">
+                {btnServices}
+              </Button>
             </div>
           </div>
           <div className={s.hero_ill}>
@@ -37,6 +43,11 @@ const Hero = () => {
         </div>
         <Brands />
       </Container>
+      <ModalOrder
+        variant="signup"
+        show={showOrder}
+        onHide={() => setShowOrder(false)}
+      />
     </section>
   )
 }
