471957c5c5be0bd5d840f9c06306c8b74311ceae
Taras <taras.galaburda@outlook.com>
2022-07-20 11:40:39 +0300

    feat: add react-query, add ability to show insta video thumbnail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 
 import Home from '~containers/Home'
 
-const HomePage = () => {
-  return <Home />
-}
+const queryClient = new QueryClient()
+
+const HomePage = () => (
+  <QueryClientProvider client={queryClient}>
+    <Home />
+  </QueryClientProvider>
+)
 
 export default HomePage
