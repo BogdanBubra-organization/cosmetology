43bb0552bfeb4369b000659043124be9801f6c58
Taras <taras.galaburda@outlook.com>
2022-08-03 16:57:35 +0300

    feat: add Quote, Collab and Founder sections for About Us page

---
+++ src/containers/About/components/Quote/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import QUOTE from './constants'
+
+import * as s from './Quote.module.scss'
+
+const Quote = () => {
+  return (
+    <Container className={s.quote_wrapper}>
+      <blockquote cite="cosmetology.ua" className={s.quote}>
+        <h2 className={s.quote_title}>{QUOTE}</h2>
+      </blockquote>
+    </Container>
+  )
+}
+
+export default Quote
