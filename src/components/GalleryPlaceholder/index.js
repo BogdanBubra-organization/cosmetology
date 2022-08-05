22c8a7de39a3dba2ff9d147ed530022894271691
Taras <taras.galaburda@outlook.com>
2022-08-05 16:17:42 +0300

    fix: add Ratio and create GalleryPlaceholder component

---
+++ src/components/GalleryPlaceholder/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Placeholder, Ratio } from 'react-bootstrap'
+
+const GalleryPlaceholder = () => {
+  return (
+    <Ratio aspectRatio="4x5">
+      <Placeholder />
+    </Ratio>
+  )
+}
+
+export default GalleryPlaceholder
