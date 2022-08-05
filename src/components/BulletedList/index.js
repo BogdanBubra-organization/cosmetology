2b74c4ad8957410c515cf3555f59b5128ee6cd94
Taras <taras.galaburda@outlook.com>
2022-08-05 11:57:55 +0300

    feat: fill up main services and change Service template

--- src/components/BulletedList/index.js
+++ src/components/BulletedList/index.js
@@ -3,12 +3,13 @@ import cn from 'classnames'
 
 import * as s from './BulletedList.module.scss'
 
-const BulletedList = ({ list = [], isLink, className }) => {
+const BulletedList = ({ list = [], isLink, withPunctuation, className }) => {
   return (
     <ul className={cn(s.bulletedlist, { [className]: className })}>
-      {list.map((el) => (
+      {list.map((el, i) => (
         <li className={s.bulletedlist_item} key={el}>
           {isLink ? <a href={`tel:${el}`}>{el}</a> : el}
+          {withPunctuation && i + 1 < list.length ? ';' : '.'}
         </li>
       ))}
     </ul>
