d00b0cdb1b0ddd135d72657cb35b5a44170474f0
Taras <taras.galaburda@outlook.com>
2022-08-05 17:22:39 +0300

    feat: add adaptive for services page

--- src/containers/Services/components/ServicesList/components/Category/index.js
+++ src/containers/Services/components/ServicesList/components/Category/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cn from 'classnames'
+import { Collapse } from 'react-bootstrap'
 import Cards from '~components/Cards'
+import useMatchMedia from '~hooks/useMatchMedia'
 
 import * as s from './Category.module.scss'
 
 const Category = ({ category, list }) => {
+  const [open, setOpen] = useState(true)
+  const isMobile = useMatchMedia('(max-width: 767px)')
+
   return (
-    <div className={s.category}>
-      <h4>{category.title}</h4>
-      <p className={s.category_descr}>{category.description}</p>
-      <Cards data={list} />
+    <div className={cn(s.category, { [s.show]: !isMobile || open })}>
+      {isMobile ? (
+        <button
+          type="button"
+          className={s.category_collapseBtn}
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <h4>{category.title}</h4>
+        </button>
+      ) : (
+        <h4>{category.title}</h4>
+      )}
+
+      <Collapse in={!isMobile || open}>
+        <div className={s.category_wrapper}>
+          <p className={s.category_descr}>{category.description}</p>
+          <Cards data={list} variant="cards" />
+        </div>
+      </Collapse>
     </div>
   )
 }
