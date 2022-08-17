bb4db62724bc85e065568b08dba5bfd7ff2c4876
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-17 08:28:27 +0300

    feat: connect Reviews page to datocms

--- src/containers/Reviews/index.js
+++ src/containers/Reviews/index.js
@@ -6,19 +6,15 @@ import S from '~components/seo'
 import { Container } from 'react-bootstrap'
 import ReviewsList from '~components/ReviewsList'
 import * as s from './Reviews.module.scss'
-import DATA from './constants'
 
-const Home = () => {
-  const { title } = DATA
-  return (
-    <Layout>
-      <S title={title} />
-      <Container as="section" className={s.reviews}>
-        <h1 className="h2">{title}</h1>
-        <ReviewsList className={s.reviews_list} />
-      </Container>
-    </Layout>
-  )
-}
+const Reviews = ({ heading, reviews }) => (
+  <Layout>
+    <S title={heading} />
+    <Container as="section" className={s.reviews}>
+      <h1 className="h2">{heading}</h1>
+      <ReviewsList reviews={reviews} className={s.reviews_list} />
+    </Container>
+  </Layout>
+)
 
-export default Home
+export default Reviews
