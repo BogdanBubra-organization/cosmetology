05cda31e09cb70653286d242da8c16e22d04ceef
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-17 07:10:07 +0300

    feat: connect Homepage hero to datocms

--- src/components/Brands/index.js
+++ src/components/Brands/index.js
@@ -1,50 +1,24 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
-import DATA from './constants'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 import * as s from './Brands.module.scss'
 
-const Brands = ({ isWithTitle }) => {
-  const { title, list } = DATA
-
-  const data = useStaticQuery(graphql`
-    query {
-      allFile(filter: { relativeDirectory: { regex: "/brands/" } }) {
-        edges {
-          node {
-            name
-            childImageSharp {
-              gatsbyImageData(quality: 100, height: 48, placeholder: NONE)
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const images = data?.allFile.edges.reduce(
-    (acc, { node }) => ({
-      ...acc,
-      [node.name]: node,
-    }),
-    {}
-  )
-
+const Brands = ({ title, list }) => {
   return (
     <div className={s.brands}>
-      {isWithTitle && (
+      {title && (
         <span className={s.brands_title} data-appear="hero">
           {title}
         </span>
       )}
       <div className={s.brands_list_wrapper}>
         <ul className={s.brands_list} data-array="hero" data-direction="bottom">
-          {list.map(({ key, alt }) => (
+          {list?.map(({ name, logo }) => (
             <GatsbyImage
-              key={key}
+              key={name}
               loading="eager"
-              image={images[key].childImageSharp.gatsbyImageData}
-              alt={alt}
+              image={getImage(logo)}
+              alt={name}
             />
           ))}
         </ul>
