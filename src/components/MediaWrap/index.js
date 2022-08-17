b90f9e679b7e73e2fa700d9044b3a41c0ceddafa
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-17 16:54:16 +0300

    feat: connect Service page to datocms

--- src/components/MediaWrap/index.js
+++ src/components/MediaWrap/index.js
@@ -3,10 +3,10 @@ import cn from 'classnames'
 
 import * as s from './MediaWrap.module.scss'
 
-const MediaWrap = ({ media, className, children }) =>
+const MediaWrap = ({ media, children, className }) =>
   media?.video ? (
     <video
-      className={cn(s.mediawrap, { [className]: className })}
+      className={cn(s.mediawrap, className)}
       src={media?.url}
       loop
       autoPlay
