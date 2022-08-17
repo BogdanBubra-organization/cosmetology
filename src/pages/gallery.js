03bb04863110fda4576a4e9c9a5d27bd22b14902
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-17 09:11:28 +0300

    feat: connect Gallery page to datocms

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,14 +1,36 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Gallery from '~containers/Gallery'
 
 const queryClient = new QueryClient()
 
-const GalleryPage = ({ location }) => (
+const GalleryPage = ({ data, location }) => (
   <QueryClientProvider client={queryClient}>
-    <Gallery location={location} />
+    <Gallery {...data.datoCmsGallery} location={location} />
   </QueryClientProvider>
 )
+
+export const query = graphql`
+  query GalleryPageQuery {
+    datoCmsGallery {
+      heading
+      instagramTab
+      resultsTab
+      media {
+        url
+        video {
+          thumbnailUrl
+        }
+        gatsbyImageData(
+          placeholder: NONE
+          forceBlurhash: true
+          imgixParams: { fit: "crop", auto: "compress,format" }
+        )
+      }
+    }
+  }
+`
 
 export default GalleryPage
