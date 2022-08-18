92217ca4afbb4e297a13a243e6f24a4789830f75
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-18 16:39:58 +0300

    feat: connect Contacts page to datocms

---
+++ src/containers/Contacts/components/PhonesList/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import * as s from './PhonesList.module.scss'
+
+const PnonesList = ({ list }) => (
+  <ul className={s.bulletedlist}>
+    {list?.map((el) => (
+      <li className={s.bulletedlist_item} key={el?.phone}>
+        <a href={`tel:${el?.phone}`}>{el?.phone}</a>
+      </li>
+    ))}
+  </ul>
+)
+
+export default PnonesList
