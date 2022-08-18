aadcfe7846279e56c4cf7257068777708b17ca34
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-18 16:39:58 +0300

    feat: connect Contacts page to datocms

--- src/containers/Contacts/index.js
+++ src/containers/Contacts/index.js
@@ -1,23 +1,25 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import cn from 'classnames'
 
 import Layout from '~components/Layout'
 import S from '~components/seo'
-import cn from 'classnames'
-
-import { Container } from 'react-bootstrap'
 import Lights from '~components/Lights'
-import { contactsTitle, contactsWrapper } from './Contacts.module.scss'
 import ContactsMap from './components/ContactsMap'
 import ContactsInfo from './components/ContactsInfo'
+import { contactsTitle, contactsWrapper } from './Contacts.module.scss'
 
-const Contacts = () => (
+const Contacts = ({ heading, map, phonesBlock, socialsBlock }) => (
   <Layout>
     <Lights />
     <S title="Наші контакти" />
     <Container className={contactsWrapper}>
-      <h1 className={cn('h2', contactsTitle)}>Наші контакти</h1>
-      <ContactsMap />
-      <ContactsInfo />
+      <h1 className={cn('h2', contactsTitle)}>{heading}</h1>
+      <ContactsMap {...map[0]} />
+      <ContactsInfo
+        phonesBlock={phonesBlock[0]}
+        socialsBlock={socialsBlock[0]}
+      />
     </Container>
   </Layout>
 )
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,6 +1,37 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 import Contacts from '~containers/Contacts'
 
-const ContactsPage = () => <Contacts />
+const ContactsPage = ({ data }) => <Contacts {...data.datoCmsContactsPage} />
+
+export const query = graphql`
+  query ContactsPageQuery {
+    datoCmsContactsPage {
+      heading
+      map {
+        title
+        address {
+          title
+          href
+        }
+        link
+      }
+      phonesBlock {
+        title
+        phones {
+          phone
+        }
+      }
+      socialsBlock {
+        title
+        socials {
+          name
+          href
+        }
+      }
+    }
+  }
+`
 
 export default ContactsPage
