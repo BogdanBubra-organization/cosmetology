8d1bc7f11d4beeb816dde5715848838c695d724a
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-19 15:07:53 +0300

    feat: connect Messengers page to datocms

--- src/containers/Messengers/index.js
+++ src/containers/Messengers/index.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import cn from 'classnames'
+
 import Layout from '~components/Layout'
 import S from '~components/seo'
 import Social from '~components/Social'
 import Pics from './components/Pics'
-import DATA from './constants'
 import * as s from './Messengers.module.scss'
 
-const Messengers = () => {
-  return (
-    <Layout isMessengersPage>
-      <S {...DATA} />
-      <Container as="section" className={s.messengers}>
-        <div className={cn(s.messengers_inner, 'animate')}>
-          <p className={s.messengers_descr}>{DATA.description}</p>
-          <Social
-            variant="messengers"
-            isWithIcon
-            iconSize={20}
-            isBtn
-            isWithText
-          />
-        </div>
-      </Container>
-      <Pics />
-    </Layout>
-  )
-}
+const Messengers = ({ text, socials, leftImage, rightImage }) => (
+  <Layout isMessengersPage>
+    <S title="Звʼязок" description={text} />
+    <Container as="section" className={s.messengers}>
+      <div className={cn(s.messengers_inner, 'animate')}>
+        <p className={s.messengers_descr}>{text}</p>
+        <Social
+          data={socials}
+          variant="messengers"
+          isWithIcon
+          iconSize={20}
+          isBtn
+          isWithText
+        />
+      </div>
+    </Container>
+    <Pics leftImage={leftImage} rightImage={rightImage} />
+  </Layout>
+)
 
 export default Messengers
