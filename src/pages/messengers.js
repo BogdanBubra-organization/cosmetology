94cb87b9a2961b6b858bc23c830fc49c3ab3bd36
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-19 15:07:53 +0300

    feat: connect Messengers page to datocms

--- src/pages/messengers.js
+++ src/pages/messengers.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 import Messengers from '~containers/Messengers'
 
-const MessengersPage = () => {
-  return <Messengers />
+const MessengersPage = ({ data }) => {
+  return <Messengers {...data.datoCmsMessengersPage} />
 }
 
+export const query = graphql`
+  query MessengersPageQuery {
+    datoCmsMessengersPage {
+      text
+      socials {
+        name
+        href
+        isExternal
+      }
+      leftImage {
+        gatsbyImageData(
+          width: 318
+          placeholder: NONE
+          forceBlurhash: true
+          imgixParams: { fit: "crop", auto: "compress,format" }
+        )
+      }
+      rightImage {
+        gatsbyImageData(
+          width: 328
+          placeholder: NONE
+          forceBlurhash: true
+          imgixParams: { fit: "crop", auto: "compress,format" }
+        )
+      }
+    }
+  }
+`
 export default MessengersPage
