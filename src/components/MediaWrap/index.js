11810607294353f03cbffd0b43c5659a24c7341f
O.Kuc_ <kucherenko.front@gmail.com>
2022-08-19 13:31:54 +0300

    feat: add videos as media

---
+++ src/components/MediaWrap/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import cn from 'classnames'
+
+import * as s from './MediaWrap.module.scss'
+
+const MediaWrap = ({ media, className, children }) =>
+  media?.video ? (
+    <video
+      className={cn(s.mediawrap, { [className]: className })}
+      src={media?.url}
+      loop
+      autoPlay
+      muted
+    />
+  ) : (
+    children
+  )
+
+export default MediaWrap
