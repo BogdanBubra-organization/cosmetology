8e59e8d5e2b59bdc97272a7312958a06ff81306b
Savelii Lukash <SaveliiLukash@gmail.com>
2022-09-14 14:22:42 +0300

    feat: add flexible pricing settings

--- src/templates/Product/components/ProductHero/index.js
+++ src/templates/Product/components/ProductHero/index.js
@@ -6,6 +6,7 @@ import cn from 'classnames'
 
 import MediaWrap from '~components/MediaWrap'
 
+import { formatPrice } from './utils'
 import * as s from './ProductHero.module.scss'
 
 const ProductHero = ({
@@ -13,54 +14,63 @@ const ProductHero = ({
   example,
   descr,
   price,
+  priceFrom,
+  priceTo,
   imageTitle,
   descrTitle,
   previewImage,
   isPriceVisible,
-}) => (
-  <section className={s.producthero}>
-    <div className={s.producthero_ill_wrapper}>
-      <div className={s.producthero_ill}>
-        <h5>{imageTitle}</h5>
-        {example ? (
-          <Ratio aspectRatio={15 / 17}>
-            <MediaWrap media={example}>
-              <GatsbyImage
-                className={s.producthero_pic}
-                image={getImage(example)}
-                alt={name}
-              />
-            </MediaWrap>
-          </Ratio>
-        ) : (
-          <img src={previewImage?.url} alt={name} />
+}) => {
+  const showAnyPrice = price || priceTo || priceFrom
+  const showExactPrice = !priceTo && !priceFrom
+
+  return (
+    <section className={s.producthero}>
+      <div className={s.producthero_ill_wrapper}>
+        <div className={s.producthero_ill}>
+          <h5>{imageTitle}</h5>
+          {example ? (
+            <Ratio aspectRatio={15 / 17}>
+              <MediaWrap media={example}>
+                <GatsbyImage
+                  className={s.producthero_pic}
+                  image={getImage(example)}
+                  alt={name}
+                />
+              </MediaWrap>
+            </Ratio>
+          ) : (
+            <img src={previewImage?.url} alt={name} />
+          )}
+        </div>
+        {isPriceVisible && !!showAnyPrice && (
+          <div className={s.producthero_content}>
+            <h3 className="h5">Вартість</h3>
+            <span className={cn('h2', s.price)}>
+              {!!priceFrom && `від ${formatPrice(priceFrom)}`}{' '}
+              {!!priceTo && `до ${formatPrice(priceTo)}`}
+              {showExactPrice && formatPrice(price)} грн
+            </span>
+          </div>
         )}
       </div>
-      {isPriceVisible && (
-        <div className={s.producthero_content}>
-          <h3 className="h5">Вартість</h3>
-          <span className={cn('h2', s.price)}>
-            {(price || 0).toFixed(2)} грн
-          </span>
+
+      <div className={s.producthero_content}>
+        <h3 className="h5">{descrTitle}</h3>
+        <StructuredText data={descr?.value} />
+
+        <div className={s.producthero_btn}>
+          <Button
+            href="https://cbox.mobi/go/cosmetology-ua"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Записатись на процедуру
+          </Button>
         </div>
-      )}
-    </div>
-
-    <div className={s.producthero_content}>
-      <h3 className="h5">{descrTitle}</h3>
-      <StructuredText data={descr?.value} />
-
-      <div className={s.producthero_btn}>
-        <Button
-          href="https://cbox.mobi/go/cosmetology-ua"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Записатись на процедуру
-        </Button>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default ProductHero
---
+++ src/templates/Product/components/ProductHero/utils.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const formatPrice = (price) => (price || 0).toFixed(2)
