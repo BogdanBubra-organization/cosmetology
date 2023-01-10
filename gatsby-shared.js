3177649b3ac29379b03461ef99149a2c922a03f6
Sasha <sasha@min.studio>
2023-01-10 14:20:39 +0200

    feat: add binotel widget

---
+++ gatsby-shared.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Script } from 'gatsby'
+
+const wrapPageElement = ({ element }) => {
+  return (
+    <>
+      {element}
+      <Script>
+        {`
+          (function (d, w, s) {
+            var widgetHash = '31u86oxavevvl478yngs',
+                gcw = d.createElement(s);
+            gcw.type = 'text/javascript';
+            gcw.async = true;
+            gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
+            var sn = d.getElementsByTagName(s)[0];
+            sn.parentNode.insertBefore(gcw, sn);
+          })(document, window, 'script');
+        `}
+      </Script>
+    </>
+  )
+}
+
+export default wrapPageElement
