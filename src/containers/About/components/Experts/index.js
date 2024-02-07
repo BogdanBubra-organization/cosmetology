fa71ac55789fb2717a6d8f615268c7f479c5420f
Animetalist <animetalist@gmail.com>
2024-02-07 18:20:39 +0200

    feat: add experts' random sorting

--- src/containers/About/components/Experts/index.js
+++ src/containers/About/components/Experts/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Container } from 'react-bootstrap'
 
@@ -6,12 +6,20 @@ import * as s from './Experts.module.scss'
 import ExpertsItem from './components/ExpertsItem'
 
 const Experts = ({ title, list }) => {
+  const [experts, setExperts] = useState([])
+
+  useEffect(() => {
+    const getRandomNumber = () => Math.random() - 0.5
+    const randomizedList = [...list].sort(getRandomNumber)
+    setExperts(randomizedList)
+  }, [])
+
   return (
     <Container as="section" className={s.experts}>
       <h2 className={s.experts_title}>{title}</h2>
 
       <div className={s.experts_list}>
-        {list.map((item) => (
+        {experts.map((item) => (
           <ExpertsItem key={item.title} {...item} />
         ))}
       </div>
